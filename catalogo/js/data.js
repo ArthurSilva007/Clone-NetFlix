0df76045735e3160920dcc0989ddecd6afa334ee
ArthurSilva007 <andersonarthur740@gmail.com>
2026-03-27 23:08:29 -0300

    feat: adiciona interface netflix e pagina de catalogo

---
+++ catalogo/js/data.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Épicos",
+        items: [
+            {
+                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+            },
+            {
+                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+            {
+                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/bb6167717c7a918c1db2bac52234f191024624dcdb41e382a2041959898d789c.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=m-CvfthZ4zg" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW5RL61ii6KWEHUy7A37SR99VLLqatLjjexA&s", top10: true, youtube: "https://www.youtube.com/watch?v=bEJI-DfxIxY" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTP2oDp1eXdfjU957hn9ySKu5Qq3IHVrKal1kUaJUXJcxcvoMgr9JHIsxFnU93IeJ0MHDYXuv6Hf6Jq_uxEoleQm4FPuLs3LNJAOA4PQBM&s", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=bjqEWgDVPe0" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ__PnG8AThsSZWMGCNna4gX9cvTScPyE6nJA&s", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=ZLW2jkd6E7g" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_AAbNnEBYU_FxGKzped3V1p5Z_KPKji6Rqt3Gsur1GySe61YLkajLxZWsVucVBStxb_y5aDoUuVMBNZqSaPsF1s44SsiJ2lBtudkj2LgX&s=10", top10: true, youtube: "https://www.youtube.com/watch?v=r6a1pdxhJ7o" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ__PnG8AThsSZWMGCNna4gX9cvTScPyE6nJA&s", top10: true, youtube: "https://www.youtube.com/watch?v=ZLW2jkd6E7g" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZWb4HQhdVLZyxEiYBUZl4WmOfR3TViLZoZA&s", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=EgUNjiuSsaI" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl-Uqe6MacS6zE3FCULe_p_63nVMWuGApcjw&s", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=HIbwgbbJzSs" },
+        ]
+    }
+];
